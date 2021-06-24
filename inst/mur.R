6d47e3a1080d8754f213d7376d0ee69c2bd772cb
Ben Tupper <btupper@bigelow.org>
2021-06-24 11:09:48 -0400

    test new list and namespace handling update test scripts

--- inst/mur.R
+++ inst/mur.R
@@ -1,8 +1,10 @@
 # MUR https://podaac-opendap.jpl.nasa.gov/opendap/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/
 #     https://podaac-opendap.jpl.nasa.gov/opendap/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/catalog.xml
 
+library(thredds)
+
 top_uri <- 'https://podaac-opendap.jpl.nasa.gov/opendap/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/catalog.xml'
-Top <- thredds::CatalogNode$new(top_uri, prefix = "thredds")
+Top <- thredds::CatalogNode$new(top_uri) #, prefix = "thredds")
 
 d271 <- Top$get_catalogs("2020")[[1]]$get_catalogs("271")[[1]]
 
--- inst/obpg.R
+++ inst/obpg.R
@@ -1,15 +1,16 @@
 # OBPG example
 
-if (FALSE){
-  top_uri <- 'https://oceandata.sci.gsfc.nasa.gov/opendap/catalog.xml'
-  Top <- thredds::CatalogNode$new(top_uri, prefix = "thredds")
-  
-  L3 <- Top$get_catalogs("MODISA")[["MODISA"]]$get_catalogs()
-  catalog2009 <- L3[[1]]$get_catalogs("2009")
-  catalog20 <- catalog2009[['2009']]$get_catalogs("020")
-  
-  chl <- catalog20[['020']]$get_datasets("A2009020.L3m_DAY_CHL_chlor_a_9km.nc")
-  
-  base_uri <- "https://oceandata.sci.gsfc.nasa.gov:443/opendap"
-  uri <- paste0(base_uri, chl[["A2009020.L3m_DAY_CHL_chlor_a_9km.nc"]]$url)
-}
+library(thredds)
+
+top_uri <- 'https://oceandata.sci.gsfc.nasa.gov/opendap/catalog.xml'
+Top <- thredds::CatalogNode$new(top_uri) #, prefix = "thredds")
+
+L3 <- Top$get_catalogs("MODISA")[["MODISA"]]$get_catalogs()
+catalog2009 <- L3[[1]]$get_catalogs("2009")
+catalog20 <- catalog2009[['2009']]$get_catalogs("020")
+
+chl <- catalog20[['020']]$get_datasets("A2009020.L3m_DAY_CHL_chlor_a_9km.nc")
+
+base_uri <- "https://oceandata.sci.gsfc.nasa.gov:443/opendap"
+uri <- paste0(base_uri, chl[["A2009020.L3m_DAY_CHL_chlor_a_9km.nc"]]$url)
+
