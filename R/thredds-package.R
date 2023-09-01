8dcce9639c104ebe0735c39e3d1f60e112be46e9
eblondel <emmanuel.blondel1@gmail.com>
2023-09-01 11:01:07 +0200

    Fix Rdoc :rocket: :package: on CRAN

--- R/thredds-package.R
+++ R/thredds-package.R
@@ -1,9 +1,12 @@
-#' thredds: Crawler for Navigating THREDDS Catalogs
-#'
-#' A limited crawler for programmatically navigating THREDDS catalogs.
-#'
 #' @name thredds
+#' @aliases thredds-package
+#' @aliases thredds
 #' @docType package
+#' 
+#' @title thredds: Crawler for Navigating THREDDS Catalogs
+#'
+#' @description A limited crawler for programmatically navigating THREDDS catalogs.
+#'
 #' @import R6
 #' @import rlang
 #' @import httr
